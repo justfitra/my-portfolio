de47d3d4421461ebf90a5f4d3644fb63e062cef4
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-08-31 02:04:56 +0700

    add new backgroun in navbar

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   };
   return (
     <>
-      <nav className="bg-[#1D1B38] fixed top-0 z-20 w-full max-w-7xl px-8 py-8 flex lg:hidden">
+      <nav className="bg-primary fixed top-0 z-20 w-full max-w-7xl px-8 py-8 flex lg:hidden">
         <div>
           <ul className="flex justify-between gap-11 ">
             {[
@@ -39,20 +39,15 @@ const Navbar = () => {
                   className="relative pb-1 group items-center"
                   scroll={false}
                 >
-                  {/* <div
-                    className={`w-10 py-0 h-[1px] group-hover:border-b ${
-                      hash === item.href ? "border-b bg-gray-200" : "bg-gray-400"
-                    } group-hover:bg-gray-200 transition-all ease-in-out duration-200 mr-3`}
-                  ></div> */}
                   <div
-                    className={`text-sm tracking-widest font-semibold group-hover:text-gray-200 group-hover:border-b-2  transition-all ease-in-out duration-200 ${
-                      hash === item.href ? "text-gray-200" : "text-gray-400"
+                    className={`text-sm tracking-widest font-semibold group-hover:text-tertiary group-hover:border-b-2  transition-all ease-in-out duration-200 ${
+                      hash === item.href ? "text-tertiary" : "text-gray-400"
                     }`}
                   >
                     {item.label}
                   </div>
                   <span
-                    className={`absolute left-0 -bottom-3 h-[2px] w-full bg-white transform transition-all duration-300 scale-x-0 group-hover:scale-x-100 origin-left ${
+                    className={`absolute left-0 -bottom-3 h-[2px] w-full bg-tertiary transform transition-all duration-300 scale-x-0 group-hover:scale-x-100 origin-left ${
                       hash === item.href ? "scale-x-100" : ""
                     }`}
                   ></span>
